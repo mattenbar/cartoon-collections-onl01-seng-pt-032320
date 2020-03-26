5d73530db1ee58046183089829325092b100720c
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 15:55:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.map do|x|
-    if array.include?(cheese_types) then puts x}
+    if array.include?(cheese_types) then puts x
   end
 end
