9e854a9a90916a38dc4b1b906521e9094d46f473
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 13:58:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  puts array.each_with_index(array, index){|x| puts "#{index+1}, #{array}" }
+  array.each_with_index(array, index){|name, index| puts "#{index+1}, #{name}" }
 end
 
 def summon_captain_planet# code an argument here
