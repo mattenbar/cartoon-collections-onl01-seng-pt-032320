64ecd544588e7bdf2a050abeb34055ddaf35e47b
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 16:00:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.map do|x|
-    if array.include?(cheese_types) then puts x
-  end
+  array.find {|x| cheese_types,include?(x)}
 end
